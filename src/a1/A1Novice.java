4065e6feeee19c61f176228c6c3e9dce1f3b7cb4
Siddu Chowdary <siddu@live.unc.edu>
2019-08-28 20:56:35 -0400

    Implemented A1Novice part.

--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -8,7 +8,40 @@ public class A1Novice {
 		
 		Scanner scan = new Scanner(System.in);
 
-		// Your code follows here.
+		//read number of customers
+		int numCustomers = scan.nextInt();
+		
+		//looping through for each customer
+		for (int i=0; i < numCustomers; i++) {
+			//read customer first name
+			String firstName = scan.next();
+			
+			//read customer last name
+			String lastName = scan.next();
+			
+			//read number of items bought
+			int numItems = scan.nextInt();
+			
+			//total price for customer
+			double totalPrice = 0.0;
+			//loop through for items bought
+			for(int j=0; j<numItems; j++) {
+				//Read quantity bought
+				int itemQuantity = scan.nextInt();
+				
+				//Read item name
+				String itemName = scan.next();
+				
+				//Read price of item
+				double itemPrice = scan.nextDouble();
+				
+				//Calculate total price for customer
+				totalPrice += itemPrice * (double)itemQuantity;
+			}
+			
+			//print output for each customer
+			System.out.println(firstName.charAt(0) + ". " + lastName + ": " + String.format("%.2f", totalPrice));
+		}
 		
 	}
 }
